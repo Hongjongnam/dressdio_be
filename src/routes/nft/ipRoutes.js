9307942dd5c75d861808f5f495a9a7a57853200b
hongjongnam <jongnam0309@gmail.com>
2025-06-11 15:39:51 +0900

    [fix capital]

--- src/routes/nft/ipRoutes.js
+++ src/routes/nft/ipRoutes.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const ipController = require("../../controllers/nft/IpController");
+const ipController = require("../../controllers/nft/ipController");
+
+/**
+ * @route GET /api/nft/ip
+ * @desc Get all IP NFTs from database
+ * @access Public
+ */
+router.get("/", ipController.getAllIPs);
+
+/**
+ * @route GET /api/nft/ip/:walletAddress
+ * @desc Get IP NFT information from blockchain by wallet address
+ * @access Public
+ */
+router.get("/:walletAddress", ipController.getIPByWalletAddress);
+
+/**
+ * @route GET /api/nft/ip/db/:walletAddress
+ * @desc Get IP NFT information from database by wallet address
+ * @access Public
+ */
+router.get("/db/:walletAddress", ipController.getIP);
 
 /**
  * @route POST /api/nft/ip/mint
  * @desc Mint a new IP NFT
  * @access Public
  */
-router.post("/mint", ipController.mintIPNFT);
-
-/**
- * @route GET /api/nft/ip
- * @desc Get all IP NFTs
- * @access Public
- */
-router.get("/", ipController.getAllIPNFTs);
+router.post("/mint", ipController.mintIP);
 
 /**
  * @route GET /api/nft/ip/:tokenId
