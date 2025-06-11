d11f8781e2bb096d89e71482ed507810a627fc12
hongjongnam <jongnam0309@gmail.com>
2025-06-11 16:56:28 +0900

    [delete] ipContract

--- src/routes/nft/index.js
+++ src/routes/nft/index.js
@@ -3,21 +3,11 @@ const router = express.Router();
 
 // Import NFT type routes
 const sbtRoutes = require("./sbtRoutes");
-const ipRoutes = require("./ipRoutes");
 
 /**
  * @route /api/nft/sbt
  * @desc SBT (Soulbound Token) related routes
  */
 router.use("/sbt", sbtRoutes);
-
-/**
- * @route /api/nft/ip
- * @desc IP NFT related routes
- */
-router.use("/ip", ipRoutes);
-
-// TODO: Add other NFT type routes (Merchandise, etc.)
-// router.use('/merchandise', merchandiseRoutes);
 
 module.exports = router;
